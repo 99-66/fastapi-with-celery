bf9273743d88a545512dd5e0fbc5eb954f1a3976
99-66 <31076511+99-66@users.noreply.github.com>
2022-02-08 01:13:37 +0900

    used celery-beat

--- project/config.py
+++ project/config.py
@@ -13,8 +13,15 @@ class BaseConfig:
     # Celery
     broker_url: str = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
+    beat_schedule: dict = {
+        "task-schedule-work": {
+            "task": "task_schedule_work",
+            "schedule": 50.0    # 50 sec
+        }
+    }
 
     WS_MESSAGE_QUEUE:str = os.environ.get("WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0")
+
 
 class DevelopmentConfig(BaseConfig):
     pass
